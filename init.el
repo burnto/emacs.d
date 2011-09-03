911c73548c2fec80c56ac70578749cff4785fcb1
Brent Fitzgerald <burnto@gmail.com>
2011-09-03 09:22:58 -0700

    Add clojure bits to el-get

--- init.el
+++ init.el
@@ -24,34 +24,46 @@
 (setq package-archives (cons '("tromey" . "http://tromey.com/elpa/") package-archives))
 (package-initialize)
 
+;; (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
+;; (require 'el-get)
+
+;; ;; Packages
+
+;; (setq el-get-sources
+;;       '((:name ruby-mode 
+;;                :type elpa
+;;                :load "ruby-mode.el")
+;;         (:name inf-ruby  :type elpa)
+;;         (:name ruby-compilation :type elpa)
+;;         (:name css-mode :type elpa)
+;;         (:name textmate
+;;                :type git
+;;                :url "git://github.com/defunkt/textmate.el"
+;;                :load "textmate.el")
+;;         (:name rvm
+;;                :type git
+;;                :url "http://github.com/djwhitt/rvm.el.git"
+;;                :load "rvm.el"
+;;                :compile ("rvm.el")
+;;                :after (lambda() (rvm-use-default)))
+;;         (:name rhtml
+;;                :type git
+;;                :url "https://github.com/eschulte/rhtml.git"
+;;                :features rhtml-mode)
+;;         (:name yaml-mode 
+;;                :type git
+;;                :url "http://github.com/yoshiki/yaml-mode.git"
+;;                :features yaml-mode)))
+;; (el-get 'sync)
+
+
 (add-to-list 'load-path "~/.emacs.d/el-get/el-get")
 (require 'el-get)
 
-;; Packages
 
-(setq el-get-sources
-      '((:name ruby-mode 
-               :type elpa
-               :load "ruby-mode.el")
-        (:name inf-ruby  :type elpa)
-        (:name ruby-compilation :type elpa)
-        (:name css-mode :type elpa)
-        (:name textmate
-               :type git
-               :url "git://github.com/defunkt/textmate.el"
-               :load "textmate.el")
-        (:name rvm
-               :type git
-               :url "http://github.com/djwhitt/rvm.el.git"
-               :load "rvm.el"
-               :compile ("rvm.el")
-               :after (lambda() (rvm-use-default)))
-        (:name rhtml
-               :type git
-               :url "https://github.com/eschulte/rhtml.git"
-               :features rhtml-mode)
-        (:name yaml-mode 
-               :type git
-               :url "http://github.com/yoshiki/yaml-mode.git"
-               :features yaml-mode)))
-(el-get 'sync)
+(setq my-packages
+      (append
+       '(clojure-mode slime swank-clojure)
+       (mapcar 'el-get-source-name el-get-sources)))
+
+(el-get 'sync my-packages)
